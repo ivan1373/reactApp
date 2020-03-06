e9d1ec09b22c877939a3ab944f7137837adf5f3c
jpinjuh <josip.pinjuh19@gmail.com>
2020-03-06 12:28:21 +0100

    fetch

--- src/list/List.js
+++ src/list/List.js
@@ -8,9 +8,7 @@ import Grid from '@material-ui/core/Grid';
 export default class List extends Component {
 
     state = {
-        items: [
-            'item1' 
-        ]
+        items: []
     }
 
     addItem = item => {
@@ -23,6 +21,26 @@ export default class List extends Component {
         this.setState({
             items: this.state.items.filter(item => item !== text)
         })
+    }
+
+    getData = () => {
+        fetch("http://192.168.2.41:5000/items")
+          .then(res => res.json())
+          .then(
+            (result) => {
+                console.log(result)
+              this.setState({
+                items: result.ITEMS
+              });
+            },
+            (error) => {
+              console.log(error)
+            }
+        )
+    }
+
+    componentDidMount() {
+        
     }
 
     render() {
